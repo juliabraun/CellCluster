036665bd2503b8168622c3820a8f6b9b43b22b9e
juliabraun <64228614+juliabraun@users.noreply.github.com>
2020-05-14 00:17:18 +0200

    test set with predefined clustering, kmeans applied

--- CellCluster/CellCluster.py
+++ CellCluster/CellCluster.py
@@ -1,12 +1,61 @@
 import clustering.kmeans_detailed as clust
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 #HERE the code__________________________
-pos = np.array([[10, 32], [12, 31], [15, 30], [20, 0], [21, -2]])
-eps = 1.0
 
-max_iter = 10
 
-centr = np.array([[20,3], [5,3], [0, 10]])
 
-clust.julia_kmeans(pos, centr, eps, max_iter)
+#d changes the scaling from [0,1) to [0,d). b translates the set of points by px + bx, py + by. 
+
+
+def box(d,b,c):
+  box1 = np.random.rand(2000,2) - 0.5
+  box1 = d*box1
+  box1[:,0] = box1[:,0] + b
+  box1[:,1] = box1[:,1] + c
+  return box1
+
+box1 = box(5,10,-10)
+box2 = box(4,8,-10)
+box3 = box(3,5,-9)
+
+box_all = np.concatenate((box1, box2, box3), axis = 0)
+
+
+eps = 0.01
+max_iter = 2000
+
+centr = np.array([[2,-10], [5,-11], [8,-9]])
+
+centr, closest_cluster = clust.julia_kmeans(box_all, centr, eps, max_iter, clust.dist_euclidean)
+
+plt.subplot(2,2,2)
+plt.scatter(box_all[:,0], box_all[:,1])
+plt.title('Points to be clustered')
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.subplot(2,2,3)
+plt.scatter(box1[:,0], box1[:,1])
+plt.scatter(box2[:,0], box2[:,1])
+plt.scatter(box3[:,0], box3[:,1])
+plt.scatter(centr[:,0], centr[:,1])
+plt.title('Predefined clusters')
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.subplot(2,2,4)
+for i in range(len(centr)):
+  cluster_0 = box_all[closest_cluster == i]
+  plt.scatter(cluster_0[:,0], cluster_0[:,1])
+
+plt.title('Points after applied clustering')
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.show()
+
+
+
--- CellCluster/clustering/kmeans_detailed.py
+++ CellCluster/clustering/kmeans_detailed.py
@@ -5,10 +5,31 @@ import math
 
 
 #HERE Functions ________________________
+
+
+# - this function calculates the manhattan distance between two points (e.g. point and cluster center). 
+#           It returns a positive real number given two vectors. 
+# - input: 
+#     - ele1, ele2: numpy arrays of length pos.shape[1].
+# - output:
+#     - sum: the manhattan distance as sum of the distances of the point components: |x1 - x2| + |y1 - y2|.
+
 def dist_manhattan(ele1, ele2):
   dist1 = np.abs(ele1 - ele2)
   sum = np.sum(dist1)
   return sum
+
+
+
+def dist_euclidean(ele1, ele2):
+  dist2 = np.abs(ele1 - ele2)
+  squared = np.power(dist2, 2)
+  sum = np.sum(dist2)
+  root = np.sqrt(sum)
+  return root
+
+
+
 
 # This function calculates the closest cluster for each point inside a set of points.
 # Centroids centr of the clusters must be provided.
@@ -19,6 +40,9 @@ def dist_manhattan(ele1, ele2):
 #   - centr: numpy.array as matrix containing the set of centroids of the clusters. 
 #       centr.shape[0] must be the number of centroids. 
 #       centr.shape[1] is the number of elements defining each centroid.
+#   - dist: a function that returns a positive real number given two vectors. 
+#           It will be used for calculating the distance between two points. 
+#           In this function, the points are numpy arrays of length pos.shape[1].
 # output: 
 #   - closest_cluster: a numpy.array of length pos.shape[0] that contains the cluster 
 #                   number corresponding to each point in pos.
@@ -28,7 +52,7 @@ def dist_manhattan(ele1, ele2):
 #   - centr.shape[1] must be equal to pos.shape[1]. 
 #    
 
-def assign_closest_cluster(pos, centr):
+def assign_closest_cluster(pos, centr, dist):
   if(centr.shape[1] != pos.shape[1]):
     print("error in \"def closest_cluster(pos, centr)\" shape of pos and centr incompatible")
     return -1, -1
@@ -38,13 +62,13 @@ def assign_closest_cluster(pos, centr):
   while i_pos < pos.shape[0]:
     i_c = 0
     ele1 = pos[i_pos,:]
-    sum = dist_manhattan(ele1, centr[i_c,:])
+    sum = dist(ele1, centr[i_c,:])
     closest_cluster[i_pos] = i_c
     dist_point_cluster[i_pos] = sum
     i_c=i_c+1
     while i_c < centr.shape[0]:
       ele1 = pos[i_pos,:]
-      sumnew = dist_manhattan(ele1, centr[i_c,:])
+      sumnew = dist(ele1, centr[i_c,:])
   
       if (sumnew<sum) == True:
         sum = sumnew
@@ -93,13 +117,16 @@ def update_centr(pos, centr, closest_cluster):
 #       centr.shape[0] must be the number of centroids. 
 #       centr.shape[1] is the number of elements defining each centroid.
 #   - centr_old: same specification of centr, contains the values of the previous clustering round.
+#   - dist: a function that returns a positive real number given two vectors. 
+#           It will be used for calculating the distance between two points. 
+#           In this function, the points are numpy arrays of length pos.shape[1].
 # output:
 #   - new_eps: A float representing the sum of (distances between each old centroid and the new centroid, respectively).
-def accuracy(centr, centr_old):
+def accuracy(centr, centr_old, dist):
   new_eps = 0
   iterator = range(centr.shape[0])
   for i in iterator:
-    dist_centr_old = dist_manhattan(centr_old[i,:], centr[i,:])
+    dist_centr_old = dist(centr_old[i,:], centr[i,:])
     if math.isnan(dist_centr_old) == False: 
       new_eps = new_eps + dist_centr_old
   return new_eps
@@ -121,6 +148,9 @@ def accuracy(centr, centr_old):
 #   - eps: A float representing the required sum of (distances between each old centroid and the new centroid, respectively).
 #         when this accuracy is reached, clustering stops. 
 #   - max_iter: an integer number specifying the maximum number of clustering iterations.
+#   - dist: a function that returns a positive real number given two vectors. 
+#           It will be used for calculating the distance between two points. 
+#           In this function, the points are numpy arrays of length pos.shape[1].
 
 # output:
 #   - centr: numpy.array as matrix containing the set of centroids of the clusters. 
@@ -129,31 +159,36 @@ def accuracy(centr, centr_old):
 #   - closest_cluster: a numpy.array of length pos.shape[0] that contains the cluster 
 #                   number corresponding to each point in pos.
 
-def julia_kmeans(pos, centr, eps, max_iter):
-  k=0
+def julia_kmeans(pos, centr, eps, max_iter, dist):
+  m = 0
   new_eps = eps + 1
   eps_track = []
 
   centr_old = np.zeros(centr.shape)
   centr = centr.astype(float)
-  while new_eps > eps:
+  while new_eps > eps and m < max_iter:
     # find cluster assignment
-    closest_cluster, dist_point_cluster = assign_closest_cluster(pos, centr)
+    closest_cluster, dist_point_cluster = assign_closest_cluster(pos, centr, dist)
     centr_old[:,:] = centr[:,:]
     #print(centr_old)
 
     # update the centroid values
     centr = update_centr(pos, centr, closest_cluster)
     # compute accuracy (change in cluster position)
-    new_eps = accuracy(centr, centr_old)
+    new_eps = accuracy(centr, centr_old, dist)
     eps_track.append(new_eps)
-    k=k+1
+    m = m + 1
 
   #print(eps_track)
 
-
+  plt.figure(figsize = (35, 35))
+  plt.subplot(2,2,1)
   plt.plot(eps_track, marker="s", ls = '')
-  plt.show()
+  plt.title('Change of accuracy eps')
+  plt.xlabel('Number of clustering round')
+  plt.ylabel('eps')
   return centr, closest_cluster
 
+
+#d changes the scaling from [0,1) to [0,d). b translates the set of points by px + bx, py + by. 
 
